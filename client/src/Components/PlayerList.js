cdb36adf8dfeaa651898d304b3703959b9343630
Arielle Hollingsworth <arizephyr123@gmail.com>
2019-10-04 08:58:42 -0700

    Answers.md completed, PlayerList.js draft

---
+++ client/src/Components/PlayerList.js
@@ -0,0 +1,58 @@
+import React from "react";
+import axios from "axios";
+import { Table } from 'reactstrap';
+import ListRow from "./DisplayedList"
+
+class PlayerList extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+            rankData: [],
+        };
+    }
+
+    componentDidMount(){
+        axios.get("http://localhost:5000/api/players")
+        .then(res => {
+            console.log(res);
+            // this.setState({
+            //     rankData: res
+            // });
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    }
+
+    render(){
+        return(
+          <>
+          <h2>Women's World Cup players ranked in search interest <br></br>June-July 2019</h2>
+            <Table>
+            <thead className="table-header">
+              <tr>
+                <th>Rank</th>
+                <th>Name</th>
+                <th>Country</th>
+                <th>Percentile</th>
+              </tr>
+            </thead>
+            <tbody>
+             {this.state.rankData.map(each => {
+            console.log(each);
+            return (
+              <ListRow
+                key={each.id}
+                rankData={each}
+              />
+            );
+          })}
+          </tbody>
+            </Table>
+            </>
+        )
+    }
+
+}
+
+export default PlayerList;
