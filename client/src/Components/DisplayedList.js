d91ef04dfe3b123436c363fa7f76964748d2bef5
Arielle Hollingsworth <arizephyr123@gmail.com>
2019-10-04 09:43:49 -0700

    fixed rank orderon display in DisplayedList.js

--- client/src/Components/DisplayedList.js
+++ client/src/Components/DisplayedList.js
@@ -4,10 +4,12 @@ import React from "react";
 const ListRow = props => {
     //console.log(props);
 
+    const formatRank = current => current + 1;
+
     return(
         
           <tr>
-            <th scope="row">{props.rankData.id}</th>
+            <th scope="row">{formatRank(props.rankData.id)}</th>
             <td>{props.rankData.name}</td>
             <td>{props.rankData.country}</td>
             <td>{props.rankData.searches}</td>
